59f90df8ec763628f31a6daccce470430a2ab584
sidalei <suchao1991@gmail.com>
2015-05-26 10:37:05 +0800

    first commit

--- BUBBLE-SORT/main.c
+++ BUBBLE-SORT/main.c
@@ -10,6 +10,26 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    printf("Hello, World!\n");
+    printf("BUBBLE SORT.\n");
+    
+    
+    int A[] = {2,3,1,5,6,8,9,4,7,10};
+    int len = 10;
+    
+    for (int i=0; i<len; i++) {
+        for (int j=len-1; j>i; j--) {
+            if (A[j]<A[j-1]) {
+                int tmp = A[j];
+                A[j] = A[j-1];
+                A[j-1] = tmp;
+            }
+        }
+    }
+    
+    
+    for (int i=0; i<len; i++) {
+        printf("%d",A[i]);
+    }
+    
     return 0;
 }
